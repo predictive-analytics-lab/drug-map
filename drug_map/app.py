04e7e3e8edf1b36be54c0fb14bb9da8ad8384f07
Bradley-Butcher <bradleydpbutcher@gmail.com>
2021-09-07 12:11:11 +0100

    added herkou caching

--- drug_map/app.py
+++ drug_map/app.py
@@ -22,11 +22,34 @@ external_stylesheets = ["https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/s
 
 app = dash.Dash('Drug map', server=server, external_stylesheets=external_stylesheets)
 
-cache = Cache(app.server, config={
-    'CACHE_TYPE': 'filesystem',
-    'CACHE_DIR': 'cache'
-})
+cache = Cache()
 
+cache_servers = os.environ.get('MEMCACHIER_SERVERS')
+if cache_servers == None:
+    cache.init_app(app, config={'CACHE_TYPE': 'simple'})
+else:
+    cache_user = os.environ.get('MEMCACHIER_USERNAME') or ''
+    cache_pass = os.environ.get('MEMCACHIER_PASSWORD') or ''
+    cache.init_app(app,
+        config={'CACHE_TYPE': 'saslmemcached',
+                'CACHE_MEMCACHED_SERVERS': cache_servers.split(','),
+                'CACHE_MEMCACHED_USERNAME': cache_user,
+                'CACHE_MEMCACHED_PASSWORD': cache_pass,
+                'CACHE_OPTIONS': { 'behaviors': {
+                    # Faster IO
+                    'tcp_nodelay': True,
+                    # Keep connection alive
+                    'tcp_keepalive': True,
+                    # Timeout for set/get requests
+                    'connect_timeout': 2000, # ms
+                    'send_timeout': 750 * 1000, # us
+                    'receive_timeout': 750 * 1000, # us
+                    '_poll_timeout': 2000, # ms
+                    # Better failover
+                    'ketama': True,
+                    'remove_failed': 1,
+                    'retry_timeout': 2,
+                    'dead_timeout': 30}}})
 # cache.clear() # remove when deploy
 
 timeout = 60 * 60 * 24 * 7 # 7 days
